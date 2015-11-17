aa95fb4521603c48b5ce1a08bdb9f8af074a15cc
Mark Fisher <mfisher@pivotal.io>
2015-11-17 08:22:55 -0500

    moving code from spring-cloud-dataflow

---
+++ src/main/java/org/springframework/cloud/dataflow/admin/spi/cloudfoundry/CloudFoundryConfiguration.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.dataflow.admin.spi.cloudfoundry;
+
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
+import org.springframework.cloud.dataflow.module.deployer.cloudfoundry.CloudFoundryModuleDeployerConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+/**
+ * Configuration which creates deployers that deploy on Cloud Foundry.
+ * Can be used either when running <i>in</i> Cloud Foundry, or <i>targeting</i> Cloud Foundry.
+ *
+ * @author Eric Bottard
+ * @author Mark Fisher
+ * @author Thomas Risberg
+ * @author Ilayaperumal Gopinathan
+ */
+@Configuration
+@Import(CloudFoundryModuleDeployerConfiguration.class)
+public class CloudFoundryConfiguration {
+
+	@Profile("cloud")
+	@AutoConfigureBefore(RedisAutoConfiguration.class)
+	protected static class RedisConfig {
+
+		@Bean
+		public Cloud cloud(CloudFactory cloudFactory) {
+			return cloudFactory.getCloud();
+		}
+
+		@Bean
+		public CloudFactory cloudFactory() {
+			return new CloudFactory();
+		}
+
+		@Bean
+		RedisConnectionFactory redisConnectionFactory(Cloud cloud) {
+			return cloud.getSingletonServiceConnector(RedisConnectionFactory.class, null);
+		}
+	}
+
+}
