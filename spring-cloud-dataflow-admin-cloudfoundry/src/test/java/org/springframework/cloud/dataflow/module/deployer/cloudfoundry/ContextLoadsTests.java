2a7c10dd0eb03679acb2752f692dd1c43796d46c
Eric Bottard <eric.bottard+github@gmail.com>
2016-02-12 16:41:55 +0100

    Fix package name

--- spring-cloud-dataflow-admin-cloudfoundry/src/test/java/org/springframework/cloud/dataflow/module/deployer/cloudfoundry/ContextLoadsTests.java
+++ spring-cloud-dataflow-admin-cloudfoundry/src/test/java/org/springframework/cloud/dataflow/module/deployer/cloudfoundry/ContextLoadsTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.cloud.dataflow.org.springframework.cloud.dataflow.module.deployer.cloudfoundry;
+package org.springframework.cloud.dataflow.module.deployer.cloudfoundry;
 
 import org.cloudfoundry.client.lib.CloudFoundryClient;
 import org.junit.Test;
